30701a3d5894b91a62864727bedb717278376ade
Nikita Tchayka <nikitatchayka@gmail.com>
2024-06-18 18:33:10 +0100

    a

--- flake.nix
+++ flake.nix
@@ -4,6 +4,7 @@
   inputs = {
     # NixOS official package source, using the nixos-23.11 branch here
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+    home-manager.url = "github:nix-community/home-manager/release-23.11";
   };
 
   outputs = { self, nixpkgs, ... }@inputs: {
