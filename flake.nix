aa2e5b2a2dd00b7974a6e2809efb69fd2212cb4c
Nikita Tchayka <nikitatchayka@gmail.com>
2024-06-18 18:40:45 +0100

    aaaaaa

--- flake.nix
+++ flake.nix
@@ -5,6 +5,7 @@
     # NixOS official package source, using the nixos-23.11 branch here
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
     home-manager.url = "github:nix-community/home-manager/release-23.11";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
     nixos-hardware.url = "github:NixOS/nixos-hardware";
   };
 
@@ -14,9 +15,17 @@
       modules = [
         # Import the previous configuration.nix we used,
         # so the old configuration file still takes effect
-        <home-manager/nixos>
-        <nixos-hardware/gpd/pocket-3>
         ./configuration.nix
+        home-manager.nixosModules.home-manager
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.users.nick = import ./home.nix;
+
+          # Optionally, use home-manager.extraSpecialArgs to pass
+          # arguments to home.nix
+        }
+        <nixos-hardware/gpd/pocket-3>
       ];
     };
   };
