16d2668f527c25a6a923fb55d5d89591066727b5
NickSeagull <git@nickseagull.dev>
2024-06-20 22:50:08 +0100

    Update

--- system/phronexia/user-level.nix
+++ system/phronexia/user-level.nix
@@ -1,28 +1,28 @@
 { config, pkgs, lib, ... }: with builtins;
 {
   imports = [
-    ../programs/git.nix
-    ../programs/vscodium.nix
-    ../programs/kitty.nix
-    ../programs/firefox.nix
-    ../programs/neovim.nix
-    ../programs/xournalpp.nix
-    ../programs/obs-studio.nix
-    ../programs/ffmpeg.nix
-    ../programs/ticktick.nix
-    ../programs/vlc.nix
-    ../programs/steam.nix
-    ../programs/obsidian.nix
-    ../programs/inkscape.nix
-    ../programs/gimp.nix
+    ../../applications/git.nix
+    ../../applications/vscodium.nix
+    ../../applications/kitty.nix
+    ../../applications/firefox.nix
+    ../../applications/neovim.nix
+    ../../applications/xournalpp.nix
+    ../../applications/obs-studio.nix
+    ../../applications/ffmpeg.nix
+    ../../applications/ticktick.nix
+    ../../applications/vlc.nix
+    ../../applications/steam.nix
+    ../../applications/obsidian.nix
+    ../../applications/inkscape.nix
+    ../../applications/gimp.nix
 
     # Services
-    ../services/redshift.nix
+    ../../services/redshift.nix
 
     # Config
-    ../config/home-manager.nix
-    ../config/session-variables.nix
-    ../config/user.nix
+    ../../config/home-manager.nix
+    ../../config/session-variables.nix
+    ../../config/user.nix
   ];
 
 }
