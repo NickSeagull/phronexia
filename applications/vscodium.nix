d9061a85b3e503b3c028e7171375442640b2b38e
NickSeagull <git@nickseagull.dev>
2024-06-21 12:03:29 +0100

    Update

--- applications/vscodium.nix
+++ applications/vscodium.nix
@@ -6,9 +6,16 @@
     package = pkgs.vscodium;
 
     extensions = with pkgs.vscode-extensions; [
+      # First things first
+      vscodevim.vim
+      
       # Keybindings suite Spacemacs style
       vspacecode.whichkey
       vspacecode.vspacecode
+
+      # Git manager, much better than the default one, as
+      # it can be controlled via keyboard
+      kahole.magit
     ];
   };
 }
--- applications/zsh.system.nix
+++ applications/zsh.system.nix
@@ -7,6 +7,8 @@
     enable = true;
     shellAliases = {
       upgrade-system = "cd $HOME/phronexia && git pull --rebase && sudo nixos-rebuild switch";
+      code = "codium";
+      gcu = "git add . && git commit -m Update"
     };
   };
 }
