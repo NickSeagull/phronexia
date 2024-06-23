89ebd96577ffb4e0c51d4ac426c919c5ceb402be
NickSeagull <git@nickseagull.dev>
2024-06-23 15:16:10 +0100

    Update

--- desktop/themes/default.nix
+++ desktop/themes/default.nix
@@ -21,19 +21,19 @@ in
     ".icons".source = "${config.gtk.theme.package}/share/icons";
   };
 
-  # xdg.configFile = {
-  #   "cinnamon".source = "${config.gtk.theme.package}/share/themes/${config.gtk.theme.name}/cinnamon";
-  #   "gnome-shell".source = "${config.gtk.theme.package}/share/themes/${config.gtk.theme.name}/gnome-shell";
-  #   "gtk-2.0".source = "${config.gtk.theme.package}/share/themes/${config.gtk.theme.name}/gtk-2.0";
-  #   "gtk-3.0".source = "${config.gtk.theme.package}/share/themes/${config.gtk.theme.name}/gtk-3.0";
-  #   "metacity-1".source = "${config.gtk.theme.package}/share/themes/${config.gtk.theme.name}/metacity-1";
-  #   "misc".source = "${config.gtk.theme.package}/share/themes/${config.gtk.theme.name}/misc";
-  #   "xfce-notify-4.0".source = "${config.gtk.theme.package}/share/themes/${config.gtk.theme.name}/xfce-notify-4.0";
-  #   "xfwm4".source = "${config.gtk.theme.package}/share/themes/${config.gtk.theme.name}/xfwm4";
-  #   "xfwm4_hidpi".source = "${config.gtk.theme.package}/share/themes/${config.gtk.theme.name}/xfwm4_hidpi";
-  #   "gtk-3.22".source = "${config.gtk.theme.package}/share/themes/${config.gtk.theme.name}/gtk-3.22";
-  #   "gtk-3.24".source = "${config.gtk.theme.package}/share/themes/${config.gtk.theme.name}/gtk-3.24";
-  #   "index.theme".source = "${config.gtk.theme.package}/share/themes/${config.gtk.theme.name}/index.theme";
-  # };
+  xdg.configFile = {
+    "cinnamon".source = "${config.gtk.theme.package}/share/themes/${config.gtk.theme.name}/cinnamon";
+    "gnome-shell".source = "${config.gtk.theme.package}/share/themes/${config.gtk.theme.name}/gnome-shell";
+    "gtk-2.0".source = "${config.gtk.theme.package}/share/themes/${config.gtk.theme.name}/gtk-2.0";
+    "gtk-3.0".source = "${config.gtk.theme.package}/share/themes/${config.gtk.theme.name}/gtk-3.0";
+    "metacity-1".source = "${config.gtk.theme.package}/share/themes/${config.gtk.theme.name}/metacity-1";
+    "misc".source = "${config.gtk.theme.package}/share/themes/${config.gtk.theme.name}/misc";
+    "xfce-notify-4.0".source = "${config.gtk.theme.package}/share/themes/${config.gtk.theme.name}/xfce-notify-4.0";
+    "xfwm4".source = "${config.gtk.theme.package}/share/themes/${config.gtk.theme.name}/xfwm4";
+    "xfwm4_hidpi".source = "${config.gtk.theme.package}/share/themes/${config.gtk.theme.name}/xfwm4_hidpi";
+    "gtk-3.22".source = "${config.gtk.theme.package}/share/themes/${config.gtk.theme.name}/gtk-3.22";
+    "gtk-3.24".source = "${config.gtk.theme.package}/share/themes/${config.gtk.theme.name}/gtk-3.24";
+    "index.theme".source = "${config.gtk.theme.package}/share/themes/${config.gtk.theme.name}/index.theme";
+  };
   
 }
