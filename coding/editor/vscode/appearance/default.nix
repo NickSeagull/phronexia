58b3b5aa7ba279204de3dfa1012fe18d97006af8
NickSeagull <git@nickseagull.dev>
2024-06-23 23:06:48 +0100

    Update

--- coding/editor/vscode/appearance/default.nix
+++ coding/editor/vscode/appearance/default.nix
@@ -2,7 +2,14 @@
 { config, pkgs, ... }:
 {
   programs.vscode.extensions = with pkgs.vscode-extensions; [
-    vscode-utils.buildVscodeMarketplaceExtension {name = "jez9999.vsclassic-icon-theme"; }
-  ];
+  ] ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
+      {
+        name = "vsclassic-icon-theme";
+        publisher = "jez9999";
+        version = "1.2.1";
+        # sha256 = "1hp6gjh4xp2m1xlm1jsdzxw9d8frkiidhph6nvl24d0h8z34w49g";
+      }
+    ];
+;
 
 }
