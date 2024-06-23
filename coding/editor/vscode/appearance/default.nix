6773ddd0dd5e282b82189ae9fb6df44a5396e538
NickSeagull <git@nickseagull.dev>
2024-06-23 23:16:41 +0100

    Update

--- coding/editor/vscode/appearance/default.nix
+++ coding/editor/vscode/appearance/default.nix
@@ -9,6 +9,17 @@
         version = "1.2.1";
         sha256 = "sha256-CtC50o3wiI8s3uj03pyZxy9l4YmbHcVuLS63TP1NIno=";
       }
+      {
+        name = "oldschool-theme";
+        publisher = "EricsonWillians";
+        version = "0.0.7";
+        sha256 = "sha256-CtC50o3wiI8s3uj03pyZxy9l4YmbHcVuLS63TP1NIno=";
+      }
     ];
+
+
+  programs.vscode.userSettings = {
+    workbench.iconTheme = "vsclassic-icon-theme";
+  };
 
 }
