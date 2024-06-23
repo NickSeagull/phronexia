20c029dbab360cfc86c51c7a8f0c7774f9013287
NickSeagull <git@nickseagull.dev>
2024-06-23 15:03:27 +0100

    Update

--- desktop/themes/default.nix
+++ desktop/themes/default.nix
@@ -1,11 +1,13 @@
 { chicago95 }:
 { pkgs, ... }:
+let pkg =chicago95.defaultPackage.x86_64-linux;
+in
 {
   gtk = {
     enable = true;
     theme = {
       name = "Chicago95";
-      package = chicago95.defaultPackage.x86_64-linux;
+      package = pkg;
     };
   };
 
@@ -13,4 +15,20 @@
     {
       GTK_THEME = "Chicago95";
     };
+
+  xdg.configFile = {
+    "cinnamon".source = "${config.gtk.theme.package}/share/themes/${config.gtk.theme.name}/cinnamon";
+    "gnome-shell".source = "${config.gtk.theme.package}/share/themes/${config.gtk.theme.name}/gnome-shell";
+    "gtk-2.0".source = "${config.gtk.theme.package}/share/themes/${config.gtk.theme.name}/gtk-2.0";
+    "gtk-3.0".source = "${config.gtk.theme.package}/share/themes/${config.gtk.theme.name}/gtk-3.0";
+    "metacity-1".source = "${config.gtk.theme.package}/share/themes/${config.gtk.theme.name}/metacity-1";
+    "misc".source = "${config.gtk.theme.package}/share/themes/${config.gtk.theme.name}/misc";
+    "xfce-notify-4.0".source = "${config.gtk.theme.package}/share/themes/${config.gtk.theme.name}/xfce-notify-4.0";
+    "xfwm4".source = "${config.gtk.theme.package}/share/themes/${config.gtk.theme.name}/xfwm4";
+    "xfwm4_hidpi".source = "${config.gtk.theme.package}/share/themes/${config.gtk.theme.name}/xfwm4_hidpi";
+    "gtk-3.22".source = "${config.gtk.theme.package}/share/themes/${config.gtk.theme.name}/gtk-3.22";
+    "gtk-3.24".source = "${config.gtk.theme.package}/share/themes/${config.gtk.theme.name}/gtk-3.24";
+    "index.theme".source = "${config.gtk.theme.package}/share/themes/${config.gtk.theme.name}/index.theme";
+  };
+  
 }
