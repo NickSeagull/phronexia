9457a41582f0d6a803bfbfdce3d677370ff46c74
NickSeagull <git@nickseagull.dev>
2024-06-24 11:31:48 +0100

    Update

--- coding/editor/vscode/language-support/default.nix
+++ coding/editor/vscode/language-support/default.nix
@@ -8,7 +8,7 @@
         name = "nix-extension-pack";
         publisher = "pinage404";
         version = "3.0.0";
-        sha256 = "sha256-CtC50o3wiI8s3uj03pyZxy9l4YmbHcVuLS63TP1NIno=";
+        sha256 = "sha256-cWXd6AlyxBroZF+cXZzzWZbYPDuOqwCZIK67cEP5sNk=";
       }
   ];
 }
