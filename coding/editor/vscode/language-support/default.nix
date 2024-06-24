b391cd83d8df011d5cc06402f335f995b406dfa5
NickSeagull <git@nickseagull.dev>
2024-06-24 23:11:46 +0100

    Update

--- coding/editor/vscode/language-support/default.nix
+++ coding/editor/vscode/language-support/default.nix
@@ -10,5 +10,23 @@
         version = "3.0.0";
         sha256 = "sha256-cWXd6AlyxBroZF+cXZzzWZbYPDuOqwCZIK67cEP5sNk=";
       }
+      {
+        name = "nix-ide";
+        publisher = "jnoortheen";
+        version = "0.3.1";
+        sha256 = "sha256-cWXd6AlyxBroZF+cXZzzWZbYPDuOqwCZIK67cEP5sNk=";
+      }
+      {
+        name = "nix-env-selector";
+        publisher = "arrterian";
+        version = "1.0.11";
+        sha256 = "sha256-cWXd6AlyxBroZF+cXZzzWZbYPDuOqwCZIK67cEP5sNk=";
+      }
+      {
+        name = "direnv";
+        publisher = "mkhl";
+        version = "0.17.0";
+        sha256 = "sha256-cWXd6AlyxBroZF+cXZzzWZbYPDuOqwCZIK67cEP5sNk=";
+      }
   ];
 }
