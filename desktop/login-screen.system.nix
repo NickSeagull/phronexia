e0cb0daeda05f3168689ee8c2241f060fc1b39f5
NickSeagull <git@nickseagull.dev>
2024-06-30 19:28:06 +0100

    Update

--- desktop/login-screen.system.nix
+++ desktop/login-screen.system.nix
@@ -2,4 +2,8 @@
 
 {
   services.xserver.displayManager.lightdm.enable = true;
+  services.xserver.displayManager.lightdm.extraConfig = ''
+    [SeatDefaults]
+    display-setup-script=xrandr --output DSI-1 --rotate right
+  '';
 }
--- hosts/phronexia/system-level.nix
+++ hosts/phronexia/system-level.nix
@@ -44,14 +44,6 @@
         '';
       }
     ];
-
-  };
-
-
-  environment.variables = {
-    # GDK_SCALE = "2";
-    # GDK_DPI_SCALE = "2";
-    # _JAVA_OPTIONS = "-Dsun.java2d.uiScale=2";
   };
 
 }
