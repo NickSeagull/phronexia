0c054fac36bf6bbd512b7ba9ff96cb51cd54a9bf
NickSeagull <git@nickseagull.dev>
2024-06-30 19:56:55 +0100

    Update

--- desktop/login-screen.system.nix
+++ desktop/login-screen.system.nix
@@ -2,7 +2,7 @@
 
 {
   services.xserver.displayManager.lightdm.enable = true;
-  services.xserver.displayManager.lightdm.extraSeatDefaults = ''
-    display-setup-script=xrandr --output DSI1 --rotate right
-  '';
+#  services.xserver.displayManager.lightdm.extraSeatDefaults = ''
+#    display-setup-script=xrandr --output DSI1 --rotate right
+#  '';
 }
