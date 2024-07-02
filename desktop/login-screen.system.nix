09d8acead2b57118a779130dda4166d1d40c0549
NickSeagull <git@nickseagull.dev>
2024-07-02 11:47:53 +0100

    Update

--- desktop/login-screen.system.nix
+++ desktop/login-screen.system.nix
@@ -3,6 +3,6 @@
 {
   services.xserver.displayManager.lightdm.enable = true;
 
-  services.xserver.displayManager.lightdm.greeter.name = "lightdm-gtk-greeter";
-  services.xserver.displayManager.lightdm.greeter.package = pkgs.lightdm-gtk-greeter;
+  # services.xserver.displayManager.lightdm.greeter.name = "lightdm-gtk-greeter";
+  # services.xserver.displayManager.lightdm.greeter.package = pkgs.lightdm-gtk-greeter;
 }
